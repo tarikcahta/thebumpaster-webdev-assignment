a27924ffed4d9f0b97a55164f26d6c633ef0f478
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-08-07 18:38:37 +0200

    Delete pages directory

--- src/app/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from '../components/Header';
-
-export const HomePage = () => {
-  return (
-    <div className="min-h-screen max-w-screen flex flex-col justify-between items-center ">
-      <Header />
-    </div>
-  );
-};
