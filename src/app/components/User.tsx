a186cd15ad4695ab5613aed0441d7f5dc36271a4
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-08-15 09:57:06 +0200

    Add link to user posts page

--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -1,5 +1,6 @@
-import { User } from '../page';
+import { User } from '../requests';
 import { Card } from 'flowbite-react';
+import Link from 'next/link';
 
 type SingleUserProps = {
   user: User;
@@ -7,13 +8,15 @@ type SingleUserProps = {
 
 export const SingleUser = ({ user }: SingleUserProps) => {
   return (
-    <Card className="max-w-sm bg-gray-700 hover:bg-gray-800" href="#">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        <p>{user.name}</p>
-      </h5>
-      <p className="font-normal text-gray-500 dark:text-gray-400">
-        <p>{user.website}</p>
-      </p>
-    </Card>
+    <Link href={`/${user.id.toString()}`}>
+      <Card className="max-w-sm bg-gray-700 hover:bg-gray-800" href="#">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>{user.name}</p>
+        </h5>
+        <p className="font-normal text-gray-500 dark:text-gray-400">
+          <p>{user.website}</p>
+        </p>
+      </Card>
+    </Link>
   );
 };
