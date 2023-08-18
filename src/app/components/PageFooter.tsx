9c3cb32f2e7ec3e4eb2deb3f1bedb4ee8c355d34
Tarik Čahtarević <tarikcahtarevic@gmail.com>
2023-08-18 10:32:45 +0200

    Fix spacing issue - footer links

--- src/app/components/PageFooter.tsx
+++ src/app/components/PageFooter.tsx
@@ -4,12 +4,12 @@ export default function PageFooter() {
   return (
     <Footer className="rounded-none  shadow-none	bg-transparent" container>
       <Footer.Copyright
-        className="text-slate-300"
+        className="text-slate-300 flex justify-center "
         by="Flowbite™"
         href="#"
         year={2023}
       />
-      <Footer.LinkGroup>
+      <Footer.LinkGroup className="flex justify-around ">
         <Footer.Link className="text-slate-300" href="#">
           About
         </Footer.Link>
